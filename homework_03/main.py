2f675eb50368318d6a0d09b85c2b26f3376dfbe2
makarovaiv <irisha-makarova_@mail.ru>
2022-10-25 21:02:37 +0300

    homework03: 'user crud' api is created

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
+from api.items import router as items_router
+from api.users.views import router as users_router
+
 app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
 
 
-@app.get("/")
+@app.get("/ping/", status_code=200)
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "pong"}
+
+
+@app.get("/hello/", status_code=200)
+def hello():
+    return {"Hello": "world!"}
